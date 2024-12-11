f1f1ae3ef5413d1c8036df96e846041118fb368d
Wellington Reis <wellington.reis@redelojacorr.com.br>
2024-12-11 20:25:35 -0300

    feat: :construction: Desafio da first decision.
    Primeiro comite do desafio da first decision.
    
    11/12/2024

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+});
